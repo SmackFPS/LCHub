9343e5003cb5a971a72458bda0882a621a398047
mikey1665 <gamermiek@gmail.com>
2014-11-24 16:21:17 -0600

    Parkour Fix

--- src/me/mike1665/parkour/CourseOne.java
+++ src/me/mike1665/parkour/CourseOne.java
@@ -23,28 +23,29 @@ import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-public class CourseOne implements Listener{
-	
-	private static Main plugin;	
-	
+public class CourseOne implements Listener {
+
+	private static Main plugin;
+
 	ArrayList<Player> join = new ArrayList<Player>();
 	ArrayList<Player> cp = new ArrayList<Player>();
-	String tag = ChatColor.RED + "" + ChatColor.BOLD + "Parkour " + ChatColor.RESET + "" + ChatColor.DARK_GRAY + "> ";
+	String tag = ChatColor.RED + "" + ChatColor.BOLD + "Parkour "
+			+ ChatColor.RESET + "" + ChatColor.DARK_GRAY + "> ";
 
-	
-	public static void initialize(Main plugin){
+	public static void initialize(Main plugin) {
 		CourseOne.plugin = plugin;
 	}
-	
+
 	@EventHandler
 	public void parkoursign(SignChangeEvent e) {
 		if (e.getLine(0).equalsIgnoreCase("[parkour1]")) {
-			
+
 			e.setLine(0, ChatColor.BOLD + "[Parkour]");
 			e.setLine(1, ChatColor.RED + "Course 1");
-			e.setLine(2, ChatColor.AQUA  + "Click to join");
-			
-		} if (e.getLine(0).equalsIgnoreCase("[leaveparkour]")) {
+			e.setLine(2, ChatColor.AQUA + "Click to join");
+
+		}
+		if (e.getLine(0).equalsIgnoreCase("[leaveparkour]")) {
 			e.setLine(0, ChatColor.BOLD + "[Leave]");
 			e.setLine(1, ChatColor.DARK_RED + "Course 1");
 		}
@@ -59,101 +60,137 @@ public class CourseOne implements Listener{
 			e.setLine(3, ChatColor.DARK_PURPLE + "you win?");
 		}
 	}
-	
+
 	@EventHandler
 	public void signparkour(PlayerInteractEvent e) {
-        if (!(e.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
-        if (e.getClickedBlock().getState() instanceof Sign) {
-            Sign s = (Sign) e.getClickedBlock().getState();
-            if (s.getLine(1).equalsIgnoreCase(ChatColor.RED + "Course 1")) {
-            	Player player = e.getPlayer();
-            		join.add(player);
-                    teleport(player);
-                    player.sendMessage(ChatColor.LIGHT_PURPLE + "" +ChatColor.BOLD + ChatColor.STRIKETHROUGH + "----------" + ChatColor.RESET + "" + ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + " Parkour " + ChatColor.LIGHT_PURPLE + "" +ChatColor.BOLD + ChatColor.STRIKETHROUGH + "----------" );
-                    player.sendMessage(tag + ChatColor.GREEN + "Welcome to the parkour course!");
-                    player.sendMessage(tag +ChatColor.GREEN + "Here you will face many challenges in this course");
-                    player.sendMessage(tag + ChatColor.GREEN + "They're Checkpoints to help you through");
-                    player.sendMessage(tag + ChatColor.GREEN + "So you wont have to start over!");
-                    player.sendMessage(tag + ChatColor.GREEN + "If you win this parkour you get to win");
-                    player.sendMessage(tag +ChatColor.GREEN + "Choice of 1000 Coins OR 100 Pixels!");
-                    player.sendMessage(ChatColor.LIGHT_PURPLE + "" +ChatColor.BOLD + "" + ChatColor.STRIKETHROUGH + "-----------------------------" );
+		if (!(e.getAction() == Action.RIGHT_CLICK_BLOCK))
+			return;
+		if (e.getClickedBlock().getState() instanceof Sign) {
+			Sign s = (Sign) e.getClickedBlock().getState();
+			if (s.getLine(1).equalsIgnoreCase(ChatColor.RED + "Course 1")) {
+				Player player = e.getPlayer();
+				join.add(player);
+				teleport(player);
+				player.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD
+						+ ChatColor.STRIKETHROUGH + "----------"
+						+ ChatColor.RESET + "" + ChatColor.LIGHT_PURPLE + ""
+						+ ChatColor.BOLD + " Parkour " + ChatColor.LIGHT_PURPLE
+						+ "" + ChatColor.BOLD + ChatColor.STRIKETHROUGH
+						+ "----------");
+				player.sendMessage(tag + ChatColor.GREEN
+						+ "Welcome to the parkour course!");
+				player.sendMessage(tag + ChatColor.GREEN
+						+ "Here you will face many challenges in this course");
+				player.sendMessage(tag + ChatColor.GREEN
+						+ "They're Checkpoints to help you through");
+				player.sendMessage(tag + ChatColor.GREEN
+						+ "So you wont have to start over!");
+				player.sendMessage(tag + ChatColor.GREEN
+						+ "If you win this parkour you get to win");
+				player.sendMessage(tag + ChatColor.GREEN
+						+ "Choice of 1000 Coins OR 100 Pixels!");
+				player.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD
+						+ "" + ChatColor.STRIKETHROUGH
+						+ "-----------------------------");
 
-            } if (s.getLine(1).equalsIgnoreCase(ChatColor.DARK_RED + "Course 1")) {
-            	Player player = e.getPlayer();
-                remove(player);
-                
-            } if (s.getLine(1).equalsIgnoreCase(ChatColor.BLUE + "Checkpoint 1")) {
-            	Player player = e.getPlayer();
-                cp.add(player);
-                player.sendMessage(tag + ChatColor.AQUA + ChatColor.BOLD +"Checkpoint 1 set!");
-                
-            } else if (s.getLine(1).equalsIgnoreCase(ChatColor.DARK_BLUE + "Course 1")) {
-            	Player player = e.getPlayer();
-            	Random object = new Random();
-            	cp.remove(player);
-            	int rewards;
+			}
+			if (s.getLine(1).equalsIgnoreCase(ChatColor.DARK_RED + "Course 1")) {
+				Player player = e.getPlayer();
+				remove(player);
 
-            	for(int counter = 1; counter<=1;counter++){
-            		rewards = 1+object.nextInt(2);
-            		
-            		if(rewards == 1) {
-                		PixlCoinsAPI.givePoints(player, 1000);
-              		  	player.playSound(player.getLocation(), Sound.LEVEL_UP, 10, 10);
-                		Bukkit.broadcastMessage(tag + ChatColor.GREEN + "" + ChatColor.BOLD + player.getName() + " Won 1000 Coins for winning parkour!");
-                		me.mike1665.coinapi.ApiEvent.scoreboard(player);
-                        teleport(player);
+			}
+			if (s.getLine(1).equalsIgnoreCase(ChatColor.BLUE + "Checkpoint 1")) {
+				Player player = e.getPlayer();
+				cp.add(player);
+				player.sendMessage(tag + ChatColor.AQUA + ChatColor.BOLD
+						+ "Checkpoint 1 set!");
 
-                	}
-                	
-            		else if(rewards == 2) {
-                		PixlPointsAPI.givePoints(player, 100);
-              		  	player.playSound(player.getLocation(), Sound.LEVEL_UP, 10, 10);
-                		Bukkit.broadcastMessage(tag + ChatColor.GREEN + ""+ ChatColor.BOLD + player.getName() + " Won 100 Pixels for winning parkour!");
-                		me.mike1665.coinapi.ApiEvent.scoreboard(player);
-                        teleport(player);
+			} else if (s.getLine(1).equalsIgnoreCase(
+					ChatColor.DARK_BLUE + "Course 1")) {
+				Player player = e.getPlayer();
+				Random object = new Random();
+				int rewards;
+				if (cp.contains(player) || join.contains(player)) {
+					for (int counter = 1; counter <= 1; counter++) {
+						rewards = 1 + object.nextInt(2);
 
-                	}
-            	}
-            }
-            	return;
-        } 
+						if (rewards == 1) {
+							PixlCoinsAPI.givePoints(player, 1000);
+							player.playSound(player.getLocation(),
+									Sound.LEVEL_UP, 10, 10);
+							Bukkit.broadcastMessage(tag + ChatColor.GREEN + ""
+									+ ChatColor.BOLD + player.getName()
+									+ " Won 1000 Coins for winning parkour!");
+							me.mike1665.coinapi.ApiEvent.scoreboard(player);
+							cp.remove(player);
+							teleport(player);
+
+						}
+
+						else if (rewards == 2) {
+							PixlPointsAPI.givePoints(player, 100);
+							player.playSound(player.getLocation(),
+									Sound.LEVEL_UP, 10, 10);
+							Bukkit.broadcastMessage(tag + ChatColor.GREEN + ""
+									+ ChatColor.BOLD + player.getName()
+									+ " Won 100 Pixels for winning parkour!");
+							me.mike1665.coinapi.ApiEvent.scoreboard(player);
+							cp.remove(player);
+							teleport(player);
+						}
+
+					}
+				} else if (!cp.contains(player) || !join.contains(player)) {
+					player.sendMessage(tag + "You must be in parkour!");
+					return;
+				}
+ 			}
+			return;
+		}
 	}
-	
+
 	@EventHandler
 	public void blockbreak(BlockBreakEvent e) {
 		Player p = e.getPlayer();
-			if (join.contains(p) || cp.contains(p)) {
-				teleport(p);
-				p.sendMessage(tag + ChatColor.DARK_RED + "Please dont break blocks in the parkour course!");
-				
-			} else if (!join.contains(p)) {
-				return;
-			}
+		if (join.contains(p) || cp.contains(p)) {
+			teleport(p);
+			p.sendMessage(tag + ChatColor.DARK_RED
+					+ "Please dont break blocks in the parkour course!");
+
+		} else if (!join.contains(p)) {
+			return;
+		}
 	}
-	
+
 	@EventHandler
 	public void parkourreturn(EntityDamageEvent event) {
 		if (join.contains(event.getEntity()) && !cp.contains(event.getEntity())) {
 			if (event.getCause() == EntityDamageEvent.DamageCause.LAVA) {
 				if ((event.getEntity() instanceof Player)) {
-						teleport((Player) event.getEntity());
-						((CommandSender) event.getEntity()).sendMessage(tag + ChatColor.GOLD + "Uh-oh You died! Teleported to the beginning!");
-						event.getEntity().eject();
-						event.getEntity().leaveVehicle();
-						event.setDamage(0);
-						return;
+					teleport((Player) event.getEntity());
+					((CommandSender) event.getEntity()).sendMessage(tag
+							+ ChatColor.GOLD
+							+ "Uh-oh You died! Teleported to the beginning!");
+					event.getEntity().eject();
+					event.getEntity().leaveVehicle();
+					event.setDamage(0);
+					return;
 				}
 			}
-			
-		}else if (cp.contains(event.getEntity()) && join.contains(event.getEntity())) {
+
+		} else if (cp.contains(event.getEntity())
+				&& join.contains(event.getEntity())) {
 			if (event.getCause() == EntityDamageEvent.DamageCause.LAVA) {
 				if ((event.getEntity() instanceof Player)) {
-						checkpoint((Player) event.getEntity());
-						((CommandSender) event.getEntity()).sendMessage(tag + ChatColor.GOLD + "Lucky! You have a checkpoint! Sent to checkpoint!");
-						event.getEntity().eject();
-						event.getEntity().leaveVehicle();
-						event.setDamage(0);
-						return;
+					checkpoint((Player) event.getEntity());
+					((CommandSender) event.getEntity())
+							.sendMessage(tag
+									+ ChatColor.GOLD
+									+ "Lucky! You have a checkpoint! Sent to checkpoint!");
+					event.getEntity().eject();
+					event.getEntity().leaveVehicle();
+					event.setDamage(0);
+					return;
 				}
 			}
 		}
@@ -161,44 +198,45 @@ public class CourseOne implements Listener{
 		event.setDamage(0);
 		event.getEntity().setFireTicks(0);
 	}
-	
+
 	public void remove(Player p) {
 		if (join.contains(p)) {
 			join.remove(p);
 			p.sendMessage(tag + ChatColor.YELLOW + "You left.");
 
-		} if (cp.contains(p)) {
+		}
+		if (cp.contains(p)) {
 			cp.remove(p);
-			
+
 		} else if (!join.contains(p) || !cp.contains(p)) {
 			p.sendMessage(tag + "You are not in parkour.");
 		}
 
 	}
-	
+
 	public void teleport(Player p) {
-		 World w = Bukkit.getWorld("world");
-		 double yaw = plugin.getConfig().getDouble("parkour1.yaw");
-		 double pitch = plugin.getConfig().getDouble("parkour1.pitch");
-         double x = plugin.getConfig().getDouble("parkour1.x");
-         double y = plugin.getConfig().getDouble("parkour1.y");
-         double z = plugin.getConfig().getDouble("parkour1.z");
-         Location parkour1 = new Location(w, x, y,z);
-         parkour1.setPitch((float) pitch);
-         parkour1.setYaw((float) yaw);
-         p.teleport(parkour1);
+		World w = Bukkit.getWorld("world");
+		double yaw = plugin.getConfig().getDouble("parkour1.yaw");
+		double pitch = plugin.getConfig().getDouble("parkour1.pitch");
+		double x = plugin.getConfig().getDouble("parkour1.x");
+		double y = plugin.getConfig().getDouble("parkour1.y");
+		double z = plugin.getConfig().getDouble("parkour1.z");
+		Location parkour1 = new Location(w, x, y, z);
+		parkour1.setPitch((float) pitch);
+		parkour1.setYaw((float) yaw);
+		p.teleport(parkour1);
 	}
-	
+
 	public void checkpoint(Player p) {
-		 World w = Bukkit.getWorld("world");
-		 double yaw = plugin.getConfig().getDouble("cp1.yaw");
-		 double pitch = plugin.getConfig().getDouble("cp1.pitch");
-        double x = plugin.getConfig().getDouble("cp1.x");
-        double y = plugin.getConfig().getDouble("cp1.y");
-        double z = plugin.getConfig().getDouble("cp1.z");
-        Location parkour1 = new Location(w, x, y,z);
-        parkour1.setPitch((float) pitch);
-        parkour1.setYaw((float) yaw);
-        p.teleport(parkour1);
+		World w = Bukkit.getWorld("world");
+		double yaw = plugin.getConfig().getDouble("cp1.yaw");
+		double pitch = plugin.getConfig().getDouble("cp1.pitch");
+		double x = plugin.getConfig().getDouble("cp1.x");
+		double y = plugin.getConfig().getDouble("cp1.y");
+		double z = plugin.getConfig().getDouble("cp1.z");
+		Location parkour1 = new Location(w, x, y, z);
+		parkour1.setPitch((float) pitch);
+		parkour1.setYaw((float) yaw);
+		p.teleport(parkour1);
 	}
 }
