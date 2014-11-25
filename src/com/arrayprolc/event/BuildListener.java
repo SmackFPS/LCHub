137cc175709e9f383ec53de2005b4d1adae6cdba
ArrayPro <arraypro07@gmail.com>
2014-11-24 22:47:01 -0500

    gamerule and event work

--- src/com/arrayprolc/event/BuildListener.java
+++ src/com/arrayprolc/event/BuildListener.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 
 import com.arrayprolc.bountifulupdate.BUtils;
 import com.arrayprolc.strings.MessageType;
@@ -24,14 +25,22 @@ public class BuildListener implements Listener {
 	public void place(BlockPlaceEvent e){
 		if(e.getPlayer().getGameMode() != GameMode.CREATIVE){
 			e.setCancelled(true);
-			BUtils.sendActionBar(e.getPlayer(), StringManager.getMessage("Please do not place blocks!", MessageType.ERROR));
+			BUtils.sendActionBar(e.getPlayer(), StringManager.getMessage("§aPlease do not place blocks!", MessageType.ERROR));
 		}
 	}
 	@EventHandler
 	public void place(BlockBreakEvent e){
 		if(e.getPlayer().getGameMode() != GameMode.CREATIVE){
 			e.setCancelled(true);
-			BUtils.sendActionBar(e.getPlayer(), StringManager.getMessage("Please do not break blocks!", MessageType.ERROR));
+			BUtils.sendActionBar(e.getPlayer(), StringManager.getMessage("§aPlease do not break blocks!", MessageType.ERROR));
+		}
+	}
+	
+	@EventHandler
+	public void onDrop(PlayerDropItemEvent e){
+		if(e.getPlayer().getGameMode() != GameMode.CREATIVE){
+			e.setCancelled(true);
+			BUtils.sendActionBar(e.getPlayer(), StringManager.getMessage("§aPlease do not drop items!", MessageType.ERROR));
 		}
 	}
 
