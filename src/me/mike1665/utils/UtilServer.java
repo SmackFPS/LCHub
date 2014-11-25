3aa2f74b06ea0ce74c7af80e14df9f7f5101df8c
ArrayPro <arraypro07@gmail.com>
2014-11-24 21:03:17 -0500

    updated to work with spigot protocol update 1.7-1.8

--- src/me/mike1665/utils/UtilServer.java
+++ src/me/mike1665/utils/UtilServer.java
@@ -1,14 +1,12 @@
 package me.mike1665.utils;
 
-import java.util.Collection;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 
 public class UtilServer {
 	
-	public static Collection<? extends Player> getPlayers() {
+	public static Player[] getPlayers() {
 		return getServer().getOnlinePlayers();
 	}
 	
