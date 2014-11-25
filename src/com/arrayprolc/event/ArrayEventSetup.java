376827a022c5e43860ab2980f790e399f511e3fa
ArrayPro <arraypro07@gmail.com>
2014-11-24 22:11:59 -0500

    fixed push

---
+++ src/com/arrayprolc/event/ArrayEventSetup.java
@@ -0,0 +1,13 @@
+package com.arrayprolc.event;
+
+import me.mike1665.Main.Main;
+
+public class ArrayEventSetup {
+
+	static Main plugin;
+	
+	public static void setupEvents(Main instance){
+		plugin = instance;
+	}
+	
+}
