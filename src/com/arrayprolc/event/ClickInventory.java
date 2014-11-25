bd710f5b4d4864e225e90bc585dff7c67c055792
ArrayPro <arraypro07@gmail.com>
2014-11-24 22:24:27 -0500

    removed ability to click on armor, so no duping is possible

--- src/com/arrayprolc/event/ClickInventory.java
+++ src/com/arrayprolc/event/ClickInventory.java
@@ -7,19 +7,25 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.InventoryType.SlotType;
+
+import com.arrayprolc.strings.MessageType;
+import com.arrayprolc.strings.StringManager;
 
 public class ClickInventory implements Listener {
-	
+
 	public Main plugin;
-	
+
 	public ClickInventory(Main instance){
 		plugin = instance;
 	}
-	
+
 	@EventHandler
 	public void click(InventoryClickEvent e){
-		if(e.getClickedInventory().getType().equals(InventoryType.CRAFTING) || e.getClickedInventory().getType().equals(InventoryType.CREATIVE)){
-			((Player) e.getWhoClicked()).sendMessage(e.getSlot() + "");
+		if(e.getSlotType().equals(SlotType.ARMOR)){
+			e.setCancelled(true);
+			e.getWhoClicked().closeInventory();
+			((Player) e.getWhoClicked()).sendMessage(StringManager.getMessage("To change armor, use the Hub Gadgets menu.", MessageType.ERROR));
 		}
 	}
 
