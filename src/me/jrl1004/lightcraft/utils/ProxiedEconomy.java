85010ade426a072af26575767beb4e1bf410ceb3
JRL1004 <Loosa.J.History6@gmail.com>
2014-11-29 20:42:45 -0700

    Fixed UUID bug

--- src/me/jrl1004/lightcraft/utils/ProxiedEconomy.java
+++ src/me/jrl1004/lightcraft/utils/ProxiedEconomy.java
@@ -8,7 +8,8 @@ import org.bukkit.entity.Player;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 
-public class ProxiedEconomy {
+public class ProxiedEconomy
+{
 	private static ProxiedEconomy instance;
 
 	public enum Currency
@@ -17,59 +18,66 @@ public class ProxiedEconomy {
 		Coins;
 	}
 
-	public ProxiedEconomy() {
+	public ProxiedEconomy()
+	{
 		instance = this;
 		Bukkit.getMessenger().registerOutgoingPluginChannel(Main.instance, "BungeeCord");
 	}
 
-	public static ProxiedEconomy getInstance() {
+	public static ProxiedEconomy getInstance()
+	{
 		return instance;
 	}
 
-	public void requestPlayerBalance(Player player, Currency c) {
+	public void requestPlayerBalance(Player player, Currency c)
+	{
 		ByteArrayDataOutput out = ByteStreams.newDataOutput();
 		out.writeUTF("Economy");
 		out.writeUTF("BALANCE");
 		out.writeUTF(c.toString());
-		out.writeUTF(player.getUniqueId().toString());
+		out.writeUTF(player.getName());
 		player.sendPluginMessage(Main.instance, "BungeeCord", out.toByteArray());
 	}
 
-	public void resetPlayerBalance(Player player, Currency c) {
+	public void resetPlayerBalance(Player player, Currency c)
+	{
 		ByteArrayDataOutput out = ByteStreams.newDataOutput();
 		out.writeUTF("Economy");
 		out.writeUTF("RESET");
 		out.writeUTF(c.toString());
-		out.writeUTF(player.getUniqueId().toString());
+		out.writeUTF(player.getName());
 		player.sendPluginMessage(Main.instance, "BungeeCord", out.toByteArray());
 	}
 
-	public void increasePlayerBalance(Player player, Currency c, Long amount) {
+	public void increasePlayerBalance(Player player, Currency c, Long amount)
+	{
 		ByteArrayDataOutput out = ByteStreams.newDataOutput();
 		out.writeUTF("Economy");
 		out.writeUTF("ADD");
 		out.writeUTF(c.toString());
-		out.writeUTF(player.getUniqueId().toString());
+		out.writeUTF(player.getName());
 		out.writeLong(amount);
 		player.sendPluginMessage(Main.instance, "BungeeCord", out.toByteArray());
 	}
 
-	public void decreasePlayerBalance(Player player, Currency c, Long amount) {
+	public void decreasePlayerBalance(Player player, Currency c, Long amount)
+	{
 		ByteArrayDataOutput out = ByteStreams.newDataOutput();
 		out.writeUTF("Economy");
 		out.writeUTF("TAKE");
 		out.writeUTF(c.toString());
-		out.writeUTF(player.getUniqueId().toString());
+		out.writeUTF(player.getName());
 		out.writeLong(amount);
 		player.sendPluginMessage(Main.instance, "BungeeCord", out.toByteArray());
 	}
 
-	public void setPlayerBalance(Player player, Currency c, Long amount) {
+	public void setPlayerBalance(Player player, Currency c, Long amount)
+	{
 		ByteArrayDataOutput out = ByteStreams.newDataOutput();
 		out.writeUTF("Economy");
 		out.writeUTF("SET");
 		out.writeUTF(c.toString());
-		out.writeUTF(player.getUniqueId().toString());
+		out.writeUTF(player.getName());
 		out.writeLong(amount);
 		player.sendPluginMessage(Main.instance, "BungeeCord", out.toByteArray());
 	}
