d61f7c534b9954b372cfe99ba08ed78507b1e921
JRL1004 <Loosa.J.History6@gmail.com>
2014-12-03 21:07:17 -0700

    Well, I tried

---
+++ src/me/jrl1004/lightcraft/gadgets/FlameTurret.java
@@ -0,0 +1,70 @@
+package me.jrl1004.lightcraft.gadgets;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import me.mike1665.particlelib.ParticleEffect;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class FlameTurret
+{
+	private final Random r;
+	private final Location gun;
+
+	public FlameTurret(final Entity entity)
+	{
+		this.gun = entity.getLocation();
+		this.r = new Random();
+		run();
+	}
+
+	public FlameTurret(final Location gun)
+	{
+		this.gun = gun;
+		this.r = new Random();
+		run();
+	}
+
+	private void run()
+	{
+		new BukkitRunnable()
+		{
+
+			@Override
+			public void run()
+			{
+				LivingEntity target = getTarget();
+				if (target == null)
+					return;
+				float xOff = (float) (gun.getX() - target.getLocation().getX());
+				float yOff = (float) (gun.getY() - target.getLocation().getY());
+				float zOff = (float) (gun.getZ() - target.getLocation().getZ());
+				ParticleEffect.FLAME.display(gun, xOff, yOff, zOff, 5, 1);
+			}
+		};
+	}
+
+	private LivingEntity getTarget()
+	{
+		List<Entity> entities = gun.getWorld().getEntities();
+		if (entities.isEmpty())
+			return null;
+		List<LivingEntity> targets = new ArrayList<LivingEntity>();
+		for (Entity e : entities)
+		{
+			if (e instanceof LivingEntity == false)
+				continue;
+			if (e.getLocation().distance(gun) > 10)
+				continue;
+			targets.add((LivingEntity) e);
+		}
+		if (targets.isEmpty())
+			return null;
+		return targets.get(r.nextInt(targets.size()));
+	}
+}
