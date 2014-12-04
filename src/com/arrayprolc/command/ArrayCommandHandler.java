5b3cf19ccf767ef05b7fabf39d028de069d59859
JRL1004 <Loosa.J.History6@gmail.com>
2014-12-03 21:09:23 -0700

    Changed test

--- src/com/arrayprolc/command/ArrayCommandHandler.java
+++ src/com/arrayprolc/command/ArrayCommandHandler.java
@@ -1,9 +1,6 @@
 package com.arrayprolc.command;
 
-import java.util.UUID;
-
-import me.jrl1004.lightcraft.utils.ProxiedEconomy;
-import me.jrl1004.lightcraft.utils.ProxiedEconomy.Currency;
+import me.jrl1004.lightcraft.gadgets.FlameTurret;
 import me.mike1665.Main.Main;
 
 import org.bukkit.Bukkit;
@@ -15,26 +12,30 @@ import org.bukkit.entity.Sheep;
 
 import com.arrayprolc.rank.RankManager;
 import com.arrayprolc.rank.ServerRank;
-import com.arrayprolc.strings.MessageType;
-import com.arrayprolc.strings.StringManager;
 
-public class ArrayCommandHandler {
+public class ArrayCommandHandler
+{
 
 	static Main plugin;
 
-	public static void setup(Main instance) {
+	public static void setup(Main instance)
+	{
 		plugin = instance;
 	}
 
-	public static boolean command(CommandSender sender, Command cmd, String label, String[] a) {
-		if (label.equalsIgnoreCase("g-reward")) {
+	public static boolean command(CommandSender sender, Command cmd, String label, String[] a)
+	{
+		if (label.equalsIgnoreCase("g-reward"))
+		{
 			if (sender instanceof Player)
 				return false;
 			Player p = Bukkit.getPlayer(a[0]);
 			int amount = Integer.parseInt(a[2]);
-			switch (a[1].toLowerCase()) {
-			
-			case "coin": {
+			switch (a[1].toLowerCase())
+			{
+
+			case "coin":
+			{
 				me.mike1665.coinapi.LcCoinsAPI.givePoints(p, amount);
 				me.mike1665.coinapi.ApiEvent.scoreboard(p);
 				return true;
@@ -45,22 +46,29 @@ public class ArrayCommandHandler {
 				return true;
 			}
 		}
-		if (label.equalsIgnoreCase("sheeplol") && ((Player) sender).isOp()) {
-			try {
+		if (label.equalsIgnoreCase("sheeplol") && ((Player) sender).isOp())
+		{
+			try
+			{
 				((Player) sender).getPassenger().eject();
 				((Player) sender).eject();
-			//	((Player) sender).spigot().
-			} catch (Exception ex) {
+				// ((Player) sender).spigot().
+			} catch (Exception ex)
+			{
 			}
-			for (Entity e : ((Player) sender).getNearbyEntities(5, 5, 5)) {
-				if (e instanceof Sheep) {
+			for (Entity e : ((Player) sender).getNearbyEntities(5, 5, 5))
+			{
+				if (e instanceof Sheep)
+				{
 					((Player) sender).setPassenger(e);
 					return false;
 				}
 			}
 		}
-		if (label.equalsIgnoreCase("rank")) {
-			if (sender instanceof Player) {
+		if (label.equalsIgnoreCase("rank"))
+		{
+			if (sender instanceof Player)
+			{
 				if (!((Player) sender).isOp())
 					return false;
 			}
@@ -69,25 +77,20 @@ public class ArrayCommandHandler {
 			RankManager.setRank(p, r);
 
 		}
-		if (label.equalsIgnoreCase("test")) {
-			Player player = (Player) sender;
-			/* 
-			 * ProxiedParty.getInstance().requestPartyHost(player.getName());
-			 * System.out.println("Host");
-			 * ProxiedParty.getInstance().requestPartySize(player.getName());
-			 * System.out.println("Size");
-			 */
-			//ProxiedParty.getInstance().requestPartyList(player);
-			ProxiedEconomy.getInstance().increasePlayerBalance(player, Currency.Coins, 10l);
-			// System.out.println("List");
+		if (label.equalsIgnoreCase("test"))
+		{
+			new FlameTurret((Player) sender);
 
 		}
 		return false;
 	}
 
-	static boolean isOnline(Player p) {
-		for (Player p2 : Bukkit.getOnlinePlayers()) {
-			if (p2.getName().equalsIgnoreCase(p.getName())) {
+	static boolean isOnline(Player p)
+	{
+		for (Player p2 : Bukkit.getOnlinePlayers())
+		{
+			if (p2.getName().equalsIgnoreCase(p.getName()))
+			{
 				return true;
 			}
 		}
