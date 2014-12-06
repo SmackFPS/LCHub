f4471d9d3146c0b3cc4c4c9a1e619d26593c17a2
ArrayPro <arraypro07@gmail.com>
2014-12-05 22:02:38 -0500

    asfasdfasdfasdfasdf

--- src/com/arrayprolc/event/ArrayEventSetup.java
+++ src/com/arrayprolc/event/ArrayEventSetup.java
@@ -26,7 +26,6 @@ public class ArrayEventSetup {
 	public static void setupEvents(Main instance){
 		PluginManager pm = Bukkit.getServer().getPluginManager();
 		plugin = instance;
-		setupFountain(instance);
 		pm.registerEvents(new ClickInventory(plugin), plugin);
 		pm.registerEvents(new TabHeaderSetup(plugin), plugin);
 		pm.registerEvents(new BuildListener(plugin), plugin);
@@ -37,56 +36,6 @@ public class ArrayEventSetup {
 		for(World w : Bukkit.getWorlds()) w.setGameRuleValue("reducedDebugInfo", "true");
 		for(World w : Bukkit.getWorlds()) w.setGameRuleValue("doDaylightCycle", "false");
 		
-	}
-	static int ticks = 0;
-	public static void setupFountain(Main instance){
-		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(instance, new Runnable(){
-			public void run(){
-				ticks++;
-				for(World w : Bukkit.getWorlds()){
-					Location fountain = new Location(w, 38, 150, 81);
-					
-					if(ticks % 2 == 0){
-						ticks = 0;
-						Material m = Material.STAINED_GLASS; 
-						spawnRandomFallingSand(m, (byte)3, fountain);
-						/*Random rand = new Random();
-						int i = rand.nextInt(1);
-						
-						switch(i){
-						case 0: spawnRandomFallingSand(m, (byte)3, fountain);
-						case 1: spawnRandomFallingSand(m, (byte)9, fountain);
-						}*/
-					}
-					for(Entity e : fountain.getWorld().getEntities()) if(e instanceof FallingBlock){
-						FallingBlock b = (FallingBlock)e;
-						for(int i = 0; i < 3; i++){
-							Location loc = b.getLocation().add(0, -i, 0);
-							if(loc.getBlock().getType() != Material.AIR){
-								if(b.getTicksLived() > 5) b.remove();
-							} 
-						}
-					}
-					return;
-				}
-
-			}
-		}, 0, 1);
-	}
-	static Random rand = new Random();
-	static void spawnRandomFallingSand(Material type, byte data, Location loc){
-	//	if(!new Location(loc.getWorld(), 38, 146, 78).getBlock().getType().equals(Material.REDSTONE_LAMP_ON)) return;
-		@SuppressWarnings("deprecation")
-		FallingBlock fall = loc.getWorld().spawnFallingBlock(loc, type, data);
-		fall.setDropItem(false);
-		fall.setVelocity(new Vector((rand.nextDouble()-0.5)/8, rand.nextDouble()*1.2, (rand.nextDouble()-0.5)/8));
-	}
-	
-	@SuppressWarnings("deprecation")
-	public static void breakBlockParticles(Block b){
-		for(Player p : Bukkit.getOnlinePlayers()){
-			p.playEffect(b.getLocation(), Effect.STEP_SOUND, b.getType().getId());
-		}
 	}
 }
 //
