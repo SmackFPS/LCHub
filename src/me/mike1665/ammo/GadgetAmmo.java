3bf62d0a667e03489ec537918e253e6f267017e3
mikey1665 <gamermiek@gmail.com>
2014-12-13 16:27:20 -0600

    Made an easy code for Gadget Ammo

---
+++ src/me/mike1665/ammo/GadgetAmmo.java
@@ -0,0 +1,51 @@
+package me.mike1665.ammo;
+
+import me.mike1665.Main.Main;
+
+import org.bukkit.OfflinePlayer;
+
+import com.arrayprolc.coin.Multiplier;
+
+public class GadgetAmmo {
+
+	private static Main plugin;
+
+	public static void initialize(Main plugin) {
+		GadgetAmmo.plugin = plugin;
+	}
+
+	public static int balanceGadgetAmo(OfflinePlayer p, String gadget) {
+		int a;
+		try {
+			a = plugin.getConfig().getInt(p.getUniqueId() + gadget);
+		} catch (Exception exc) {
+			a = 0;
+		}
+		return a;
+	}
+
+	public static boolean hasGadgetAmo(OfflinePlayer p, String gadget, int i) {
+		i = i * Multiplier.coin(p);
+		if (plugin.getConfig().getInt(p.getUniqueId() + gadget) >= i)
+			return true;
+		return false;
+	}
+
+	public static void removeGadgetAmo(OfflinePlayer p, String gadget, int amount) {
+		amount = amount * Multiplier.coin(p);
+		plugin.getConfig()
+				.set(p.getUniqueId() + gadget,
+						plugin.getConfig().getInt(p.getUniqueId() + gadget, 0)
+								- amount);
+		plugin.saveFile();
+	}
+
+	public static void addGadgetAmo(OfflinePlayer p, String gadget, int amount) {
+		amount = amount * Multiplier.coin(p);
+		plugin.getConfig()
+				.set(p.getUniqueId() + gadget,
+						plugin.getConfig().getInt(p.getUniqueId() + gadget, 0)
+								+ amount);
+		plugin.saveFile();
+	}
+}
