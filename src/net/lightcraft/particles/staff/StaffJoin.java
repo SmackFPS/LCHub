257731ec4b66c994693bc654f72d84930d12688c
mikey1665 <gamermiek@gmail.com>
2014-12-24 14:31:51 -0600

    Fixes for staff join.

--- src/net/lightcraft/particles/staff/StaffJoin.java
+++ src/net/lightcraft/particles/staff/StaffJoin.java
@@ -1,8 +1,5 @@
 package net.lightcraft.particles.staff;
 
-import net.lightcraftmc.fusebox.util.effects.EffectManager.EffectType;
-import net.lightcraftmc.fusebox.util.extra.ExtraManager;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,22 +9,22 @@ public class StaffJoin implements Listener{
 	@EventHandler
 	public void staffJoin(PlayerJoinEvent e) {
 		if (e.getPlayer().getName().equals("ArrayPro")) {
-			ExtraManager.Activate(e.getPlayer(), EffectType.Helix);
+			me.mike1665.extra.ExtraManager.Activate(e.getPlayer(), me.mike1665.effects.EffectManager.EffectType.Helix);
 		}
 		if (e.getPlayer().getName().equals("mike1665")) {
-			ExtraManager.Activate(e.getPlayer(), EffectType.Helix);
+			me.mike1665.extra.ExtraManager.Activate(e.getPlayer(), me.mike1665.effects.EffectManager.EffectType.Helix);
 		}
 		if (e.getPlayer().getName().equals("SmackFPS")) {
-			ExtraManager.Activate(e.getPlayer(), EffectType.Helix);
+			me.mike1665.extra.ExtraManager.Activate(e.getPlayer(), me.mike1665.effects.EffectManager.EffectType.Helix);
 		}
 		if (e.getPlayer().getName().equals("JRL1004")) {
-			ExtraManager.Activate(e.getPlayer(), EffectType.Helix);
+			me.mike1665.extra.ExtraManager.Activate(e.getPlayer(), me.mike1665.effects.EffectManager.EffectType.Helix);
 		}
 		if (e.getPlayer().getName().equals("Dablakbandit")) {
-			ExtraManager.Activate(e.getPlayer(), EffectType.Helix);
+			me.mike1665.extra.ExtraManager.Activate(e.getPlayer(), me.mike1665.effects.EffectManager.EffectType.Helix);
 		}
 		if (e.getPlayer().getName().equals("OlsonElliot")) {
-			ExtraManager.Activate(e.getPlayer(), EffectType.Helix);
+			me.mike1665.extra.ExtraManager.Activate(e.getPlayer(), me.mike1665.effects.EffectManager.EffectType.Helix);
 		}
 	}
 
